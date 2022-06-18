64546592c37c3410bc5cbd45f90b1b9a24401042
takumi <hiatana72@gmail.com>
2022-06-18 13:56:13 +0900

    🔰 Try using 'データ型とリテラル'

--- try/try_it_out.js
+++ try/try_it_out.js
@@ -22,3 +22,40 @@ console.log("ランチは" + lunch);
 
 var dinner = "きつねうどん";
 console.log("夕食は" + dinner);
+
+/*
+ ## データ型とリテラル
+*/
+
+console.log(typeof true); // => "boolean"
+console.log(typeof 46); // => "number"
+console.log(typeof 1996199254740992n); // => "bigint"
+console.log(typeof "これは文字列です"); // => "string"
+console.log(typeof Symbol("シンボル")); // => "symbol"
+console.log(typeof undefined); // => "undefined"
+console.log(typeof null); // => "object"
+console.log(typeof ["配列"]); // => "object"
+console.log(typeof { key: "value" }); // => "object"
+console.log(typeof function () {}); // => "function"
+
+console.log(`今日の朝ごはんは${morning}でした。`);
+
+// undefinedが宣言できる(非推奨)
+var undefined = "独自の未定義値"; // undefinedという名前の変数をエラーなく定義できる
+console.log(undefined); // => "独自の未定義値"
+
+// オブジェクト
+const apple = {
+  jp_name: "りんご",
+  color: "red", // ドキュメント上ではキーもダブルクォートで囲われていたが、Pritterが外している
+  shape: "sphere",
+};
+console.log(`りんごの色は${apple.color}で形は${apple.shape}です。`);
+
+// 配列
+const tdd_steps = [
+  "テストケースを作成する",
+  "テスト結果をグリーンにする",
+  "リファクタリングする",
+];
+console.log(`ToDoは「${tdd_steps[2]}」です。`);
