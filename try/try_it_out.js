afd2ea2f7b2adaf3f55558c225f203a181b16027
takumi <hiatana72@gmail.com>
2022-06-18 10:34:21 +0900

    🔰 Output defined variables

--- try/try_it_out.js
+++ try/try_it_out.js
@@ -14,8 +14,11 @@
 */
 
 const morning = "フレンチトースト";
+console.log("朝食は" + morning);
 
 let lunch = "カレー";
 lunch = "家系ラーメン";
+console.log("ランチは" + lunch);
 
 var dinner = "きつねうどん";
+console.log("夕食は" + dinner);
